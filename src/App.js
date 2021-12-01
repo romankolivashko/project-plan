c1c6b7d300b6b2dc9cfd15d4ed400adcaf09bc28
Roman Kolivashko <rkolivashko@gmail.com>
2021-11-30 17:09:54 -0800

    Added Navbar links

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-
+import { BrowserRouter } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Project Planning</h1>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+      </div>
+    </BrowserRouter>
   );
 }
 
