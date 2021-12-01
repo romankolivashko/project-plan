801b71dbac622df08b399cc9a52eb9d0bc28a254
Roman Kolivashko <rkolivashko@gmail.com>
2021-12-01 14:38:38 -0800

    Added CreateNewProject form

--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 class  SignUp extends React.Component {
   state = {
--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Notifications from './Notifications';
 import ProjectList from '../projects/ProjectList'
 
